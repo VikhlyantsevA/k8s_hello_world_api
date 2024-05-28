6be7ce7748e12e15cf2af5d046cc4644a4cead0d
Aleksander Vikhlyantsev <alexgidro91@mail.ru>
2024-05-28 12:55:14 +0700

    updated Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -6,6 +6,6 @@ COPY k8s_hello_world.py requirements.txt .
 
 RUN pip3 install -r ./requirements.txt
 
-EXPOSE 5001:5000
+EXPOSE 5000
 
 CMD ["python3", "./k8s_hello_world.py"]
--- k8s_hello_world.py
+++ k8s_hello_world.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/hello_world")
 def hello_world():
-    return f"<p>Version1: Hello, World from {socket.gethostname()}! </p>"
+    return f"<p>Version1.0.1: Hello, World from {socket.gethostname()}! </p>"
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
