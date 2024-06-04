00172a09d871a43bad4a5f40d3fe2a28e0027806
Aleksander Vikhlyantsev <alexgidro91@mail.ru>
2024-06-04 15:10:03 +0700

    v1.0.2 was added

--- k8s_hello_world.py
+++ k8s_hello_world.py
@@ -1,11 +1,20 @@
 from flask import Flask
+import requests
 import socket
 
 app = Flask(__name__)
 
-@app.route("/hello_world")
+
+@app.route("/")
 def hello_world():
-    return f"<p>Version1.0.1: Hello, World from {socket.gethostname()}! </p>"
+    return f"<p>Version1.0.3: Api was run on {socket.gethostname()}! </p>"
+
+
+@app.route("/nginx")
+def nginx_start_page():
+    url = 'http://nginx:8080'
+    return requests.get(url).text
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
